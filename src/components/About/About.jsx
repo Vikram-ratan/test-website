c4d6f3d7b55d37525444dc186e92fe72b4da4cd8
Vikram Ratan <145298333+Vikram-ratan@users.noreply.github.com>
2024-12-02 18:08:44 -0600

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
                     <div className="about-paragraph">
                         <p>Hey There! I’m a Data Analyst and currently pursuing a master’s in Business Analytics and Artificial Intelligence at the University of Texas at Dallas. With over three years of experience as both a Business and Data Analyst, I specialize in turning complex data into actionable insights and building visual dashboards that drive stakeholder decision-making.
                         </p>
-                        <p>My expertise spans identifying trends, optimizing processes, and delivering data-driven solutions that enhance business performance. Passionate about leveraging cutting-edge tools, I thrive on pushing the boundaries of data innovation, whether through predictive modeling, AI applications, or creative visualizations. Outside of work, I’m an extrovert with a love for travel, history, and taking on new adventures—and I also cook a mean biryani! If you're intrigued by data, creativity, or a good adventure, let’s connect and see where our paths might cross!
+                        <p>My expertise spans identifying trends, optimizing processes, and delivering data-driven solutions that enhance business performance. Passionate about leveraging cutting-edge tools, I thrive on pushing the boundaries of data innovation, whether through predictive modeling, AI applications, or creative visualizations. Outside of work, I’m an extrovert with a love for travel, history, and taking on new adventures—and I also cook a delicious biryani! If you're intrigued by data, creativity, or a good adventure, let’s connect and see where our paths might cross!
                         </p>
                     </div>
                     <div className="about-skills">
